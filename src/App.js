f28a736ba3321115e94699f18889358388bcbf78
MaissaBenYaala <maissa.benyaala@gmail.com>
2022-01-28 04:34:49 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { Button,Container, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Container className='container'>
+     <h1>
+       LOGIN
+     </h1>
+     <Form className='form'>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+  <FontAwesomeIcon className='icon' icon={faUser} />
+    <Form.Control className='input' type="email" placeholder="User name / Email" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+  <FontAwesomeIcon className='icon' icon={faLock} />
+    <Form.Control className='input' type="password" placeholder="Password" />
+  </Form.Group>
+  <Button className='login' variant="primary" type="submit">
+    SUBMIT
+  </Button>
+</Form>
+
+     </Container>
     </div>
   );
 }
